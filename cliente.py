dc31c7a4bc01319e5114653bcc7c839bd195085f
AndreiSystem <47399696+AndreiSystem@users.noreply.github.com>
2019-12-04 16:46:53 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -6,15 +6,15 @@ class Cliente(Pessoa):
         with open('C:/Users/900213/Desktop/pythonClt2/Exercicio06/classes/banco/clientes.txt', 'a') as arquivo:
             arquivo.write(f'{self.id}; {self.name}; {self.last_name}; {self.cpf} \n')
 
+    def listar(self):
+        lista = []
+
+        with open('C:/Users/900213/Desktop/pythonClt2/Exercicio06/classes/banco/clientes.txt', 'r') as arquivo:
+            for linha in arquivo:
+                linha = linha.strip().split(';')
+                lista.append({'id': linha[0], 'name': linha[1], 'last-name': linha[2], 'cpf': linha[3]}) 
+        
+        return lista
 
 
 
-
-
-joao = Cliente('joao', 'silva', 1231)
-paulo = Cliente('paulo', 'luca', 2343)
-juca = Cliente('juca', 'pinto', 1234)
-
-joao.cadastrar()
-paulo.cadastrar()
-juca.cadastrar()
